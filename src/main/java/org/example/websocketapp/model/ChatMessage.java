fc9b7f4ece67389d3c713558d07ddaa38bda0575
youssef145-eng <yousefsameh174@gmail.com>
2025-04-10 00:20:01 +0200

    Create model ChatMessage

---
+++ src/main/java/org/example/websocketapp/model/ChatMessage.java
@@ -0,0 +1,35 @@
+package org.example.websocketapp.model;
+
+public class ChatMessage {
+    private String message;
+    private String sender;
+    private ChatType chatType;
+
+    public ChatMessage() {
+    }
+
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public ChatType getChatType() {
+        return chatType;
+    }
+
+    public void setChatType(ChatType chatType) {
+        this.chatType = chatType;
+    }
+}
---
+++ src/main/java/org/example/websocketapp/model/ChatType.java
@@ -0,0 +1,5 @@
+package org.example.websocketapp.model;
+
+public enum ChatType {
+    JOIN,LEAVE,CHAT
+}
