716518a6fc0ca61da25231d636b43bfcd670911b
youssef145-eng <yousefsameh174@gmail.com>
2025-04-09 23:16:31 +0200

    first commit

---
+++ src/main/java/org/example/websocketapp/WebSocketAppApplication.java
@@ -0,0 +1,13 @@
+package org.example.websocketapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSocketAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebSocketAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/websocketapp/WebSocketAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.websocketapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSocketAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
